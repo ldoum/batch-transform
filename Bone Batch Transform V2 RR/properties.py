bbc2b0b55f50e4aa074f5486f76186b72252dea0
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2025-11-25 01:53:05 -0500

    Refactor properties.py unregisteration for bone transformation

--- Bone Batch Transform V2 RR/properties.py
+++ Bone Batch Transform V2 RR/properties.py
@@ -184,8 +184,9 @@ def register():
 
 def unregister():
     for cls in reversed(classes):
-        bpy.utils.register_class(cls)
+        bpy.utils.unregister_class(cls)
     del bpy.types.Scene.inst
     
 if __name__ == "__main__":
     register()
+
