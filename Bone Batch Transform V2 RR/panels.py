cd8ed1f0d993f11cec1a487157c5ca6e8ea9b57e
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2025-11-25 01:48:23 -0500

    Fix import and registration issues in panels.py

--- Bone Batch Transform V2 RR/panels.py
+++ Bone Batch Transform V2 RR/panels.py
@@ -97,7 +97,8 @@ def register():
 
 def unregister():
     for cls in reversed(classes):
-        bpy.utils.register_class(cls)
+        bpy.utils.unregister_class(cls)
 
 if __name__ == "__main__":
+
     register()
