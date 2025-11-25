8d51dc53c8277a928a420e5c34d77b4a16ea3dcc
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2025-11-25 01:44:33 -0500

    Refactor unregister method

--- Bone Batch Transform V2 RR/operators.py
+++ Bone Batch Transform V2 RR/operators.py
@@ -153,7 +153,8 @@ def register():
 
 def unregister():
     for cls in reversed(classes):
-        bpy.utils.register_class(cls)
+        bpy.utils.unregister_class(cls)
 
 if __name__ == "__main__":
+
     register()
